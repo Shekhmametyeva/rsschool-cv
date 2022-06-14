56b69e6178b49d2eb1442a22c08c79a9331f153b
Shekhmametyeva <alina-r-s98@outlook.com>
2022-06-14 13:58:45 +0400

    feat: add section skills to cv

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// бургер-меню
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
 const navList = document.querySelector('.nav__list');
@@ -22,4 +23,18 @@ function removeMenu (event) {
     if (!isMenuOpen) { return }
     if (event.target.className === 'link' || event.target === wraperDark) { toggleMenu () }
      
+}
+
+// анимация прогресса
+const skills = document.querySelector('.skills__item');
+const progress = document.querySelectorAll('.skills__progress');
+window.addEventListener('scroll', animateProgressBar);
+
+
+function animateProgressBar () {
+   if (skills.getBoundingClientRect().top < document.documentElement.clientHeight - skills.clientHeight) {
+       progress.forEach(el => el.classList.add('progress_active'));
+       window.removeEventListener('scroll', animateProgressBar);
+   }
+
 }
